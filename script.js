ed9aac57abd1f27b0a181ee1b8b653c6987a1a0a
tvnetx01 <tvnetx01@gmail.com>
2025-12-02 01:55:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+[
+  {
+    "leagueEn": "Italia: Serie A",
+    "homeName": "Bologna",
+    "awayName": "Cremonese",
+    "homeLogoUrl": "https://tg.selina45.workers.dev/https://cfcdn.xdapiym5297.com/zqwin007/Image/team/images/16486905420.png",
+    "awayLogoUrl": "https://tg.selina46.workers.dev/https://cfcdn.xdapiym5297.com/zqwin007/Image/team/images/1399/1hk6e9yjze12.png",
+    "matchId": 2784603,
+    "leagueId": 34,
+    "homeScore": 0,
+    "awayScore": 0,
+    "state": 0,
+    "matchTime_t": 1764618300000,
+    "matchTimeWIB": "02/12/2025 02:45 WIB",
+    "baseStreamUrl": "https://ppdd10.playerktidfintkd.shop/live.html?_pa={encodedParams}&_pb=4"
+  },
+  {
+    "leagueEn": "Spanyol: LaLiga",
+    "homeName": "Rayo Vallecano",
+    "awayName": "Valencia",
+    "homeLogoUrl": "https://tg.selina31.workers.dev/https://cfcdn.xdapiym5297.com/zqwin007/Image/team/images/164871283245.png",
+    "awayLogoUrl": "https://tg.selina31.workers.dev/https://cfcdn.xdapiym5297.com/zqwin007/Image/team/images/164871269140.png",
+    "matchId": 2804431,
+    "leagueId": 31,
+    "homeScore": 0,
+    "awayScore": 0,
+    "state": 0,
+    "matchTime_t": 1764619200000,
+    "matchTimeWIB": "02/12/2025 03:00 WIB",
+    "baseStreamUrl": "https://ppdd10.playerktidfintkd.shop/live.html?_pa={encodedParams}&_pb=4"
+  }
+]
